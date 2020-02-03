721d0621113ce47d5793dd76d16a5bb06153fd45
KatiusciaNovaesdeSa <54957332+KatiusciaNovaesdeSa@users.noreply.github.com>
2020-02-02 21:16:46 -0500

    Update ex2.js

--- week3/ex2.js
+++ week3/ex2.js
@@ -5,8 +5,8 @@ var convert = function convert(){
     const buf = Buffer.from([0x62,0x75,0x66,0x65,0x72]);
     buf.forEach(function(elements){
         return console.log(elements);
-    })
-
+    });
+      console.log('buffer converted')
 }
 emitter.on('convert', convert);
 emitter.emit('convert');
